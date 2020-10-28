ae75440600b300db3d6a8f9eb0589fd6a3b81f82
Christian Vilzmann <cvilzmann@googlemail.com>
2020-10-28 23:20:02 +0100

    initial

---
+++ src/components/guideline.component.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default class Guideline extends Component {
+    render () {
+        return (
+        <div>
+            <h1>Guideline component</h1>
+        </div>
+        )
+    }
+}
